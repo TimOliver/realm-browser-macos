5ad8f9d72c145bcf472da834af041fbabfd6cbee
Tim Oliver <git@tim.dev>
2026-04-18 04:28:38 +0900

    Removed sandboxing from the app

--- RealmBrowser/Models/RLMDocument.m
+++ RealmBrowser/Models/RLMDocument.m
@@ -16,7 +16,6 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-@import AppSandboxFileAccess;
 @import Realm.Private;
 
 #import "RLMDocument.h"
@@ -25,7 +24,6 @@
 
 @interface RLMDocument ()
 
-@property (nonatomic, strong) NSURL *securityScopedURL;
 @property (nonatomic, strong) NSError *error;
 
 @end
@@ -55,25 +53,10 @@
         return nil;
     }
 
-    NSURL *folderURL = fileURL.URLByDeletingLastPathComponent;
-
     self = [super init];
 
     if (self != nil) {
         self.fileURL = fileURL;
-
-        // In case we're trying to open Realm file located in app's container directory there is no reason to ask access permissions
-        if (![[NSFileManager defaultManager] isWritableFileAtPath:folderURL.path]) {
-            [[AppSandboxFileAccess fileAccess] requestAccessPermissionsForFileURL:folderURL persistPermission:YES withBlock:^(NSURL *securityScopedFileURL, NSData *bookmarkData) {
-                self.securityScopedURL = securityScopedFileURL;
-            }];
-
-            if (self.securityScopedURL == nil) {
-                return nil;
-            }
-        }
-
-        [self.securityScopedURL startAccessingSecurityScopedResource];
 
         self.presentedRealm = [[RLMRealmNode alloc] initWithFileURL:self.fileURL];
 
@@ -83,22 +66,6 @@
     }
 
     return self;
-}
-
-- (void)dealloc {
-    if (self.securityScopedURL != nil) {
-        //In certain instances, RLMRealm's C++ destructor method will attempt to clean up
-        //specific auxiliary files belonging to this realm file.
-        //If the destructor call occurs after the access to the sandbox resource has been released here,
-        //and it attempts to delete any files, RLMRealm will throw an exception.
-        //Mac OS X apps only have a finite number of open sandbox resources at any given time, so while it's not necessary
-        //to release them straight away, it is still good practice to do so eventually.
-        //As such, this will release the handle a minute, after closing the document.
-        NSURL *scopedURL = self.securityScopedURL;
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(60 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            [scopedURL stopAccessingSecurityScopedResource];
-        });
-    }
 }
 
 - (BOOL)loadByPerformingFormatUpgradeWithError:(NSError **)error {
